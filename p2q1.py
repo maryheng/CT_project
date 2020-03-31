9f9239c5edd3dc52ddd7ff4d479c456c8de78d27
zhan rong lim <zrlim.2018@smu.edu.sg>
2020-03-31 20:50:12 +0800

    hi

--- p2q1.py
+++ p2q1.py
@@ -23,15 +23,17 @@ def get_route(p, v, flags):
   return output
 
 #Gets Euclidean distance between 2 flags based on their coordinates
-def get_distance(x1,y1,x2,y2):
-  distance = 0
-  x1=float(x1)
-  y1 = float(y1)
-  x2 = float(x2)
-  y2 = float(y2)
-  distance = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
-  return distance
+# def get_distance(x1,y1,x2,y2):
+#   distance = 0
+#   x1=float(x1)
+#   y1 = float(y1)
+#   x2 = float(x2)
+#   y2 = float(y2)
+#   distance = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
+#   return distance
 
+def get_distance(node_A, node_B):
+  return ((float(node_A[2]) - float(node_B[2])) ** 2 + (float(node_A[3]) - float(node_B[3])) ** 2) ** 0.5
 
 #gets points gained per distance AKA effieciency 
 def get_effieciency(distance, points):
@@ -42,10 +44,9 @@ def test_nn_v1(p, flags):
   output = []
   current_point = 0
   i=0
-  #idk hahahha
-  flags = get_ordered_list(flags, 0 , 0)
+  sp = ["sp", 0,0,0]
+  flags = get_ordered_list(flags, sp)
   while (current_point < p):
-    #insert function? 
     output.append(flags[i])
     current_point += float(flags[i][1])
     i += 1
@@ -54,11 +55,11 @@ def test_nn_v1(p, flags):
 
 
 #ref from https://stackoverflow.com/questions/30636014/how-to-order-a-list-of-points-by-distance-to-a-given-point/30636169
-def get_ordered_list(points, x, y):
+def get_ordered_list(points, sp):
   #points.sort(key = lambda p: ((float(p[2]) - float(x))**2 + (float(p[3]) - float(y))**2)**(1/2))
   #  points.sort(key = lambda p: float(p[1])/((float(p[2]) - float(x))**2 + (float(p[3]) - float(y))**2)**(1/2), reverse=True)
   
-  points.sort(key = lambda p: get_distance(p[2],p[3],0,0))
+  points.sort(key = lambda p: get_distance(p, sp))
   # points.sort(key=lambda p: (-p[1]))
   return points
 
@@ -82,8 +83,9 @@ def two_opt(route):
 def get_route_distance(route):
   output = 0
   for i in range(len(route)-1):
-    output += get_distance(route[i][2], route[i][3], route[i+1][2], route[i+1][3])
+    output += get_distance(route[i], route[i+1])
   return output
+
 
 def deflag(flags):
   output = []
